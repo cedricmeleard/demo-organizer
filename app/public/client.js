5ff60ed70fae7b1358f42dda502d88507ea838b2
Cédric Méléard <cedric.meleard@gmail.com>
2016-05-13 17:13:17 +0200

    Optimisation et refactoring

--- app/public/client.js
+++ app/public/client.js
@@ -1,16 +1,15 @@
 /**
  * Created by cedric on 22/04/16.
  */
-//(function(){
+(function () {
     'use strict';
     var socket = io();
 
     function Organizer() {
         var self = this;
-        //load user from input and set it to model
-        self.user = ko.observable( JSON.parse( $("#usrInfo").val() ) );
-
         socket.emit('connect');
+        //load user from input and set it to model
+        self.user = ko.observable(JSON.parse($("#usrInfo").val()));
 
         self.items = ko.observableArray();
         self.tickets = ko.computed(function () {
@@ -20,30 +19,20 @@
                 });
             return tickets();
         });
-        self.moveUp = function(data){
+        self.moveUp = function (data) {
             if (data.position == 0) return;
-            var move = { from : data.position, to : data.position - 1 };
+            var move = {from: data.position, to: data.position - 1};
 
             socket.emit('item positioned', move);
         }
-        self.moveDown = function(data){
+        self.moveDown = function (data) {
             if (data.position == self.items().length - 1) return;
-            var move = { from : data.position, to : data.position + 1 };
+            var move = {from: data.position, to: data.position + 1};
 
             socket.emit('item positioned', move);
         }
-        self.delete = function(data){
+        self.delete = function (data) {
             socket.emit('item deleted', data.id);
-        }
-        self.affect = function (data) {
-            var sendData = {
-                id : data.id,
-                user: ko.toJS(self.user)
-            }
-            socket.emit('item affected', sendData);
-        }
-        self.unAffect = function (data) {
-            socket.emit('item unaffected', data.id);
         }
 
         socket.on('send items', function (items) {
@@ -65,7 +54,7 @@
         });
     }
 
-    //monitor change - emit to server
+//monitor change - emit to server
     ko.extenders.itemChange = function (target, option) {
         target.subscribe(function (newValue) {
             socket.emit('item updated', ko.toJS(option));
@@ -73,5 +62,52 @@
         return target;
     };
 
+    function ItemDemoViewModel(params) {
+        var self = this;
+        self.item = params.item;
+        self.user = params.user;
+
+        self.title = ko.computed(function () {
+            return self.item.source + ' - ' + self.item.text;
+        });
+
+        self.affect = function () {
+            var sendData = {
+                id: self.item.id,
+                user: ko.toJS(self.user)
+            }
+            socket.emit('item affected', sendData);
+        }
+        self.unAffect = function () {
+            socket.emit('item unaffected', self.item.id);
+        }
+    }
+
+    ko.components.register('item-demo', {
+        viewModel: ItemDemoViewModel,
+        template: '<h2>' +
+        '<span class="content__content-title" data-bind="text : title"></span>' +
+        '</h2>' +
+        '<div class="content__form-group">' +
+        '<textarea class="form-control" rows="4" placeholder="Entrer la description" data-bind="value : item.description"></textarea>' +
+        '</div>' +
+        '<!-- ko ifnot: item.affectedUser -->' +
+        '<button type="button" data-bind="click : affect">' +
+        '<i class="material-icons">thumb_up</i>' +
+        '</button>' +
+        '<!-- /ko -->' +
+        '<!-- ko if: item.affectedUser -->' +
+        '<div class="user">' +
+        '<!-- ko with: item.affectedUser-->' +
+        '<img class="user-img" data-bind="attr : { src: photo }" />' +
+        '<label class="user-name" data-bind="text : name" ></label>' +
+        '<!-- /ko -->' +
+        '<button type="button" data-bind="click : unAffect">' +
+        '<i class="material-icons">delete</i>' +
+        '</button>' +
+        '</div>' +
+        '<!-- /ko -->'
+    });
+
     ko.applyBindings(new Organizer());
-//})();
+})();
