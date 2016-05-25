3dac9c68f21f8991b576784fe69bda6ee6d014bd
Cédric Méléard <cedric.meleard@gmail.com>
2016-05-25 21:38:52 +0200

    woups :(

--- app/js/config.js
+++ app/js/config.js
@@ -3,8 +3,8 @@
  */
 var config = {
     googleAuth : {
-        GOOGLE_CLIENT_ID: "390863276942-ha3onskmmj98p7058e4li3pe29sr7905.apps.googleusercontent.com",
-        GOOGLE_CLIENT_SECRET: "ttSPWXvIu1Nez7rbT7nlsTsV",
+        GOOGLE_CLIENT_ID: "-- google app CLIENT ID --",
+        GOOGLE_CLIENT_SECRET: "-- google client secret --",
         GOOGLE_CALLBACK_URL: "http://localhost:81/oauth2callback"
     }
 };
